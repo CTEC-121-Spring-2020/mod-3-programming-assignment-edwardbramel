be38342b20a4d54906ebaa1d11aec3f491a9ffdd
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-05-08 12:56:14 -0700

    im stuck

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,14 +2,27 @@
 #   Programming Assignment 4
 #     Prob-1.py
 
-# <YOUR NAME>
+# Edward LaManna
+"""
+Input: ordersubtotal
+process: Calculate shipping cost. Cost is $2.99 for totals less than
+         $10.00 and zero otherwise
+output: return shipping cost
+"""
+
 
 def shippingCost(orderSubTotal):
     shippingCost = 2.99
-    # enter code here to test for free
-
+    x = input
+    y = 10.00
+    print(input("what is the total coast?: "))
+    if x > y:
+        print("the shipping cost is 0")
+    else:
+        print("the shipping cost is 2.99")
 
     return shippingCost
+
 
 def unitTest():
     print("Shipping cost if subtotal < 10.00:\t", shippingCost(5.99))
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,14 +2,29 @@
 #   Programming Assignment 4
 #     Prob-2.py
 
-# <YOUR NAME>
+# Edward Lamanna
 
 '''
 Your IPO comment goes here
 '''
 
+
 def main():
-    # your code goes here
+    name = input("what is your name?: ")
+    wage = input("How much do you make an hour?: ")
+    hours = input("in a week how many hours do you work?: ")
+    regularWage = hours * wage
+
+    """
+     print(ovetimewage)
+     print(GrossPay)
+     print(tax)
+     print(insurance)
+     print(netPay)
+     """
+    print("Employes Name:", name)
+    print("Employes regualr hours:", hours)
+    print("you regular wage is:", regularWage)
 
 
 main()
